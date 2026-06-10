df2a79869edd6d786247ec6e1d466060c519457a
Qunerum <qunerumofficial@gmail.com>
2026-06-10 16:53:25 +0200

    update 2026-06-10 16:53

--- src/main.c
+++ src/main.c
@@ -1,17 +1,11 @@
 #include "../lib/qgpu.h"
 
-float s = 30;
 void Init() {
-    loadTexture(FILES "test.qgt", 0);
 }
 void Update() {
-    if (getKey(QKEY_UP)) s++;
-    if (getKey(QKEY_DOWN) && s > 0) s--;
-
-    drawTextureScale(0, 0, 0, s);
 }
 
 int main() {
-    qgpuCreate(1280, 720, "QGPU Template Project", Init, Update);
+    qgpuCreate(1280, 720, "QEngine", Init, Update);
     return 0;
 }
